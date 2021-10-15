1205abe59a89952e110e9c7dbbdb3f3479794188
apollorion <joey@apollorion.com>
2021-10-15 09:27:42 -0400

    syntax mismatch

--- run_text.py
+++ run_text.py
@@ -69,7 +69,7 @@ def run():
                 put_last_tweet(new_id)
 
                 # Print the Tweet onto the sign
-                my_text = process_tweet(mentions, seconds=DISPLAY_MY_TWEETS_FOR_X_SECONDS))
+                my_text = process_tweet(mentions, seconds=DISPLAY_MY_TWEETS_FOR_X_SECONDS)
                 print("Ending Mention")
         else:
             #Display either the last tweet or the default text
