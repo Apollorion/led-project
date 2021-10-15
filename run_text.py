693ee0a4a20fd990bbcf0339f45b5f9054858e1c
apollorion <joey@apollorion.com>
2021-10-14 21:00:58 -0400

    twitter changes

--- run_text.py
+++ run_text.py
@@ -7,6 +7,9 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from rgbmatrix import graphics
 import tweepy
 import os
+import random
+
+DEFAULT_TEXT = "This is my LED Sign, I love it."
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"])
@@ -33,39 +36,61 @@ options.drop_privileges=False
 matrix = RGBMatrix(options = options)
 
 def run():
+    my_text = DEFAULT_TEXT
+    while True:
+
+        print("Retreiving Mentions")
+        last_id = get_last_tweet()
+        mentions = api.mentions_timeline(last_id, tweet_mode='extended')
+
+        if len(mentions) > 0:
+            for mention in reversed(mentions):
+                print("Starting new mention")
+
+                # note and store last tweet
+                new_id = mention.id
+                put_last_tweet(new_id)
+
+                # Print the Tweet onto the sign
+                my_text = mention.text.replace("@Apollorion ", "", 1)
+                display_text(my_text)
+                print("Ending Mention")
+        else:
+            #Display either the last tweet or the default text
+            display_text(my_text)
+
+
+def display_text(my_text, seconds=60):
     offscreen_canvas = matrix.CreateFrameCanvas()
     font = graphics.Font()
     font.LoadFont("fonts/7x13B.bdf")
-    textColor = graphics.Color(255, 0, 0)
+    textColor = graphics.Color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
     pos = offscreen_canvas.width
 
 
-    while True:
-        print("Retreiving Mentions")
-        mentions = api.mentions_timeline()
-        if len(mentions) == 0:
-            return
+    run_seconds = 0
+    while run_seconds < seconds:
+        offscreen_canvas.Clear()
+        length = graphics.DrawText(offscreen_canvas, font, pos, 12, textColor, my_text)
+        pos -= 1
+        if (pos + length < 0):
+            pos = offscreen_canvas.width
 
-        for mention in reversed(mentions):
-            print("Starting new mention")
+        time.sleep(0.05)
+        run_seconds += 0.05
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
 
-            new_id = mention.id
-            my_text = mention.text.replace("@Apollorion ", "", 1)
+def get_last_tweet():
+    f = open("tweet_ID.txt" 'r')
+    lastId = int(f.read().strip())
+    f.close()
+    return lastId
 
-            # Print the Tweet onto the sign
-            run_seconds = 0
-            while run_seconds < 10:
-                print("Displaying mention for 10 seconds")
-                offscreen_canvas.Clear()
-                length = graphics.DrawText(offscreen_canvas, font, pos, 12, textColor, my_text)
-                pos -= 1
-                if (pos + length < 0):
-                    pos = offscreen_canvas.width
-
-                time.sleep(0.05)
-                run_seconds += 0.05
-                offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
-            print("Ending Mention")
+def put_last_tweet(Id):
+    f = open("tweet_ID.txt", 'w')
+    f.write(str(Id))
+    f.close()
+    return
 
 try:
     print("Press CTRL-C to stop")
